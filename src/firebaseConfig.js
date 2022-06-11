7e4eda6f99e705ee941e77dce8bdf6ceb55fe466
Professor-promise <professorpromise1000@gmail.com>
2022-06-11 22:03:07 +0100

    update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -52,7 +52,7 @@ export async function upload(file, currentUser, setLoading) {
   const fileRef = ref(storage, currentUser.uid + '.png');
 
   setLoading(true);
-  const snapshot = await uploadBytes(fileRef, file);
+  await uploadBytes(fileRef, file);
   const photoURL = await getDownloadURL(fileRef);
   updateProfile(currentUser, { photoURL });
   setLoading(false);
